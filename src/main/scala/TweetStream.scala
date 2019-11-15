c555971ef3f8cba75964ec09ea0420d51658bada
DeadmanIQ445 <solovov305@gmail.com>
2019-11-16 00:54:55 +0300

    Made WC sae to file

--- src/main/scala/TweetStream.scala
+++ src/main/scala/TweetStream.scala
@@ -1,7 +1,9 @@
+import scala.reflect.io.File
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.{Row, SQLContext, SparkSession}
 import org.apache.spark.sql.streaming.{DataStreamWriter, StreamingQuery}
 import org.apache.spark.sql.functions.{current_timestamp, lit}
+
 
 object TweetStream{
   var fileWriter: StreamingQuery = _
@@ -14,16 +16,25 @@ object TweetStream{
 
     if (flag == "1"){
       modelsPath = "hdfs://namenode:9000/user/jakarta/models"
-      chkpntPath = "hdfs://namenode:9000/user/jakarta/chkpnt/"
-      outputPath = "hdfs://namenode:9000/user/jakarta/output/"
+      chkpntPath = "hdfs://namenode:9000/user/jakarta/chkpnt"
+      outputPath = "hdfs://namenode:9000/user/jakarta/output"
       trainPath =  "hdfs://namenode:9000/twitter/twitter_sentiment_data.csv"
     }
     else {
       modelsPath  = "./models"
-      chkpntPath  = "./chkpnt/"
-      outputPath = "./output/"
+      chkpntPath  = "./chkpnt"
+      outputPath = "./output"
       trainPath = "./csv/train.csv"
     }
+
+    val outputDir = File(outputPath)
+    if (outputDir.exists)
+      outputDir.deleteRecursively()
+    val ckpntDir = File(chkpntPath)
+    if (ckpntDir.exists)
+      ckpntDir.deleteRecursively()
+    chkpntPath = chkpntPath.concat("/")
+    outputPath = outputPath.concat("/")
 
     if (!scala.reflect.io.File(modelsPath.concat("/model1")).exists) {
       classifier.fit_and_evaluate(sparkSession, trainPath, modelsPath)
@@ -35,20 +46,26 @@ object TweetStream{
 
     for(i <- 1 to 4){
       var writer : DataStreamWriter[Row] = classifier.predict(stream, i.toString, modelsPath)
-        .select("features_raw", "prediction")
         .withColumn("time_stamp", lit(current_timestamp()))
         .select("time_stamp", "features_raw", "prediction")
         .writeStream
 
-      writer.format("console").option("truncate", "false").start()
+//      writer.format("console").option("truncate", "false").start()
 
       this.fileWriter = writer.format("csv")
       .option("checkpointLocation", chkpntPath.concat(i.toString))
       .option("path", outputPath.concat(i.toString)).start()
     }
 
-    WordCount.count(stream).writeStream.format("console").outputMode("complete").option("truncate", "false").start()
-
+    val wc = WordCount.count(stream).writeStream
+      .format("memory")
+      .queryName("WordCount")
+      .outputMode("complete")
+      .start()
+    while(wc.isActive){
+      Thread.sleep(30000)
+      sparkSession.sql("select * from WordCount").coalesce(1).write.mode("overwrite").csv(outputPath.concat("WS"))
+    }
     sparkSession.streams.awaitAnyTermination()
   }
 }
