5a001d11bc25cab2537b1ac909b84c04edd42992
alekseyzhuchkov.tut@gmail.com <alekseyzhuchkov.tut@gmail.com>
2019-11-19 20:31:00 +0000

    Change files' paths

--- src/main/scala/TweetStream.scala
+++ src/main/scala/TweetStream.scala
@@ -15,9 +15,9 @@ object TweetStream{
     var trainPath: String = null
 
     if (flag == "1"){
-      modelsPath = "hdfs://namenode:9000/user/jakarta/models"
-      chkpntPath = "hdfs://namenode:9000/user/jakarta/chkpnt"
-      outputPath = "hdfs://namenode:9000/user/jakarta/output"
+      modelsPath = "hdfs://namenode:9000/user/weimaraner/models"
+      chkpntPath = "hdfs://namenode:9000/user/weimaraner/chkpnt"
+      outputPath = "hdfs://namenode:9000/user/weimaraner/output"
       trainPath =  "hdfs://namenode:9000/twitter/twitter_sentiment_data.csv"
     }
     else {
@@ -44,6 +44,7 @@ object TweetStream{
     .option("host","10.90.138.32").option("port", 8989)
     .load().toDF("features_raw")
 
+
     for(i <- 1 to 4){
       var writer : DataStreamWriter[Row] = classifier.predict(stream, i.toString, modelsPath)
         .withColumn("time_stamp", lit(current_timestamp()))
@@ -57,15 +58,17 @@ object TweetStream{
       .option("path", outputPath.concat(i.toString)).start()
     }
 
-    val wc = WordCount.count(stream).writeStream
-      .format("memory")
-      .queryName("WordCount")
-      .outputMode("complete")
-      .start()
-    while(wc.isActive){
-      Thread.sleep(30000)
-      sparkSession.sql("select * from WordCount").coalesce(1).write.mode("overwrite").csv(outputPath.concat("WS"))
-    }
+
+//    val wc = WordCount.count(stream).writeStream
+//      .format("memory")
+//      .queryName("WordCount")
+
+//      .outputMode("complete")
+//      .start()
+//    while(wc.isActive){
+//      Thread.sleep(30000)
+//      sparkSession.sql("select * from WordCount").coalesce(1).write.mode("overwrite").csv(outputPath.concat("WS"))
+//    }
     sparkSession.streams.awaitAnyTermination()
   }
 }
